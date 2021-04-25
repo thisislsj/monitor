f71b1a9a6846b4344c7dd78620e4d5e268b78505
Lahiru Sandeepa <lahirusandeepa@outlook.com>
2021-04-25 21:42:57 +0530

    dashboard grid

--- monitor_frontend/lib/screens/dashboard_home_page.dart
+++ monitor_frontend/lib/screens/dashboard_home_page.dart
@@ -9,11 +9,34 @@ class _DashboardHomeState extends State<DashboardHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sensor Monitoring System"),
-      ),
+      // appBar: AppBar(
+      //   title: Text("Sensor Monitoring System"),
+      // ),
       body: Container(
-        child: Text("Dashboard"),
+        child: GridView.count(
+          crossAxisCount: 4,
+          mainAxisSpacing: 15.0,
+          crossAxisSpacing: 10.0,
+          children: List.generate(100, (index) {
+            return Center(
+              //child: Text("A"),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  height: 100,
+                  width: 200,
+                  color: Colors.orange,
+                  child: Column(
+                    children: [
+                      Text("Sensor Name"),
+                      Text("data"),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
