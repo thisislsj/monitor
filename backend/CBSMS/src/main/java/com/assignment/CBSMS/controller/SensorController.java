4d2b9d09ac9a76c226782de03a79ac9e3fbb28a0
Lahiru Sandeepa <lahirusandeepa@outlook.com>
2021-04-26 16:46:31 +0530

    CORS config backend and Dashboard card improvement

--- backend/CBSMS/src/main/java/com/assignment/CBSMS/controller/SensorController.java
+++ backend/CBSMS/src/main/java/com/assignment/CBSMS/controller/SensorController.java
@@ -6,6 +6,7 @@ import com.assignment.CBSMS.entity.Sensor;
 import com.assignment.CBSMS.service.SensorService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,17 +22,19 @@ public class SensorController {
     @Autowired
     SensorService sensorService;
 
-
+	@CrossOrigin(origins = "http://localhost:56132")
     @GetMapping(value = "")
     public List<Sensor> getAll(){
         return sensorService.findAll();
     }
 
+	@CrossOrigin(origins = "http://localhost:56132")
     @PostMapping(value = "")
     public Sensor newSensor(@RequestBody Sensor newSensor){
         return sensorService.saveOrUpdate(newSensor);
     }
 
+    @CrossOrigin(origins = "http://localhost:56132")
     @GetMapping(value = "/{id}")
     public Sensor getById(@PathVariable(value = "id") String id){
         return sensorService.findById(id);
